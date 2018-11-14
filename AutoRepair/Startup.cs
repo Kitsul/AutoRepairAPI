0abf8917aa9a7be1bf284bd476795d68620be722
Yuriy Kitsul <yuriy.kitsul@revolutioninsure.com>
2018-11-14 13:28:04 +0200

    added swagger

--- AutoRepair/Startup.cs
+++ AutoRepair/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -16,6 +17,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace AutoRepair
 {
@@ -34,16 +36,37 @@ namespace AutoRepair
             services.AddCors();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new Info
+                    {
+                        Title = "Auto Repair API",
+                        Version = "v1",
+                        Contact = new Contact()
+                        {
+                            Name = "Yuriy Kitsul",
+                            Email = "lightblok@gmail.com"
+                        }
+                    });
+
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
+
             var connectionString = Configuration["ConnectionStrings:AutoRepairDBConnectionString"];
             services.AddDbContext<AutoRepairContext>(x => x.UseSqlServer(connectionString));
 
             services.AddScoped<IAppoimtmentsRepository, AppoimtmentsRepository>();
             services.AddScoped<IModelsCarRepository, ModelsCarRepository>();
-            services.AddScoped<IUserRepository, UserRepository>(); 
+            services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<IDiscountRepository, DiscountRepository>();
             services.AddAutoMapper();
-        }
 
+            
+        }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
@@ -62,6 +85,14 @@ namespace AutoRepair
                 .AllowCredentials());
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
         }
     }
 }
